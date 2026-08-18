2ad7228a422f39ac46be0d1e1ec8b5eab5f352aa
Shlomi <shlomikaufman91@gmail.com>
2026-08-18 16:21:36 +0300

    feat: add update player at repository

--- backend/src/repositories/player.repository.js
+++ backend/src/repositories/player.repository.js
@@ -1,15 +1,19 @@
 import { createClient } from "@supabase/supabase-js";
 import { connection } from "../databases/supabase.connection.js";
 
-export default function playerRepository(connection,
-) {
+export default function playerRepository() {
+  //   /**@type {PostgrestQueryBuilder}*/ dbC,
   async function selectPlayer(id) {
-    return await connection.select("*").match({ id });
+    return await connection.select("*").eq({ id });
   }
 
-  async function insertPlayer(data) {
-    return await dbConnection.insert(data);
+  async function insertPlayer() {
+    return await connection.insert({ chips: 1000 }).select("*");
   }
 
-  return { selectPlayer, insertPlayer };
+  async function updatePlayer(id, data) {
+    return await connection.update(data).eq({ id });
+  }
+
+  return { selectPlayer, insertPlayer, updatePlayer };
 }
