bfd354cea3168214c96a3991fd92692bbe82b899
Shlomi <shlomikaufman91@gmail.com>
2026-08-18 16:05:12 +0300

    feat: add create player at repo

--- backend/src/repositories/player.repository.js
+++ backend/src/repositories/player.repository.js
@@ -1,12 +1,15 @@
 import { createClient } from "@supabase/supabase-js";
 import { connection } from "../databases/supabase.connection.js";
 
-export default function playerRepository(
-  /**@type {PostgrestQueryBuilder}*/ connection,
+export default function playerRepository(connection,
 ) {
   async function selectPlayer(id) {
     return await connection.select("*").match({ id });
   }
 
-  return { selectPlayer };
+  async function insertPlayer(data) {
+    return await dbConnection.insert(data);
+  }
+
+  return { selectPlayer, insertPlayer };
 }
