811269aab4750923fdaedccdf1caa9112c3aff6a
Shlomi <shlomikaufman91@gmail.com>
2026-08-19 16:06:35 +0300

    feat: add types cards

--- backend/src/config.js
+++ backend/src/config.js
@@ -5,3 +5,6 @@ export const {
   SECRET_KEY,
   PORT = 3000,
 } = process.env;
+
+export const VALUE_CARDS = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"];
+export const TYPE_CARDS = ["S", "H", "D", "C"];
