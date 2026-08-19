facde764985e266fc9eee63a435866b9ab64a500
Shlomi <shlomikaufman91@gmail.com>
2026-08-19 11:13:00 +0300

    fix: connection to mongoDB

--- backend/src/databases/mongodb.connection.js
+++ backend/src/databases/mongodb.connection.js
@@ -2,4 +2,21 @@ import { MongoClient } from "mongodb";
 import { MONGO_URI, NAME_DB } from "../config.js";
 
 const client = new MongoClient(MONGO_URI);
-export const db = await client.db(NAME_DB);
+let db;
+
+async function connectToMongo() {
+  if (!db) {
+    try {
+      await client.connect();
+      db = client.db(NAME_DB);
+      console.log("connect");
+    } catch (error) {
+      console.log(error.message);
+    }
+  }
+  return db;
+}
+
+await connectToMongo();
+
+export { db };
