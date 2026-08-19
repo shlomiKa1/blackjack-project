3fb946ed39e2d8df97b72d41da21b620a34deb5f
Shlomi <shlomikaufman91@gmail.com>
2026-08-19 08:08:50 +0300

    fix: supabase connection

--- backend/src/databases/supabase.connection.js
+++ backend/src/databases/supabase.connection.js
@@ -1,5 +1,4 @@
 import { createClient } from "@supabase/supabase-js";
 import { SECRET_KEY, SUPABASE_URI } from "../config.js";
 
-const supabase = createClient(SUPABASE_URI, SECRET_KEY);
-const database = supabase.from("player");
+export const supabase = createClient(SUPABASE_URI, SECRET_KEY);
