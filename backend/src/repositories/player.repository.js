bdacaefff1f73ea551c33391042b1a1484924660
Shlomi <shlomikaufman91@gmail.com>
2026-08-19 08:10:00 +0300

    fix: player repository

--- backend/src/repositories/player.repository.js
+++ backend/src/repositories/player.repository.js
@@ -1,18 +1,14 @@
-import { createClient } from "@supabase/supabase-js";
-import { connection } from "../databases/supabase.connection.js";
-
-export default function playerRepository() {
-  //   /**@type {PostgrestQueryBuilder}*/ dbC,
-  async function selectPlayer(id) {
-    return await connection.select("*").eq({ id });
+export default function createPlayerRepository(supabase) {
+  async function selectPlayer(filter) {
+    return await supabase.from("player").select("*").match(filter);
   }
 
   async function insertPlayer() {
-    return await connection.insert({ chips: 1000 }).select("*");
+    return await supabase.from("player").insert({ chips: 1000 }).select("*");
   }
 
   async function updatePlayer(id, data) {
-    return await connection.update(data).eq({ id });
+    return await supabase.from("player").update(data).eq({ id });
   }
 
   return { selectPlayer, insertPlayer, updatePlayer };
