aa134e47e1763c95e24960e6a77f2fc1d53a1ef3
Shlomi <shlomikaufman91@gmail.com>
2026-08-19 16:51:47 +0300

    feat: Add sum number in cards

--- backend/src/utils/helper.js
+++ backend/src/utils/helper.js
@@ -1,4 +1,4 @@
-import { VALUE_CARDS, TYPE_CARDS } from "../config.js";
+import { VALUE_CARDS, TYPE_CARDS, ACE, SPACIEL } from "../config.js";
 
 function openNewCard() {
   const valueRandom = Math.floor(Math.random() * VALUE_CARDS.length);
@@ -38,4 +38,28 @@ export function newRound() {
   dealerCards.push(insureNewCard(playerCards, dealerCards));
 
   return { playerCards, dealerCards };
+}
+
+export function sumNumberCard(cards) {
+  const ace = 0;
+  let total = 0;
+
+  for (const card in cards) {
+    if (card.rank === ACE) {
+      ace += 1;
+    } else if (SPACIEL.includes(card.rank)) {
+      total += 10;
+    } else {
+      total += +card.rank;
+    }
+  }
+
+  if (ace > 0) {
+    if (total + ace * 11 > 21) {
+      total += ace * 1;
+    } else {
+      total += ace * 11;
+    }
+  }
+  return total;
 }
