23ca347199740f0b368daa1f9e2820ba22199f33
Shlomi <shlomikaufman91@gmail.com>
2026-08-19 19:00:54 +0300

    feat: add hit game at controller

--- backend/src/controllers/round.controller.js
+++ backend/src/controllers/round.controller.js
@@ -1,12 +1,17 @@
 export default function createRoundController(roundService) {
   async function startRound(req, res) {
     const playerId = req.headers["x-player-id"];
-    console.log(req.body);
-
     const round = await roundService.startRound(playerId, req.body);
 
     res.status(201).send(round);
   }
 
-  return { startRound };
+  async function hit(req, res) {
+    const playerId = req.headers;
+    const hitGame = await roundService.hit(playerId);
+
+    res.send(hitGame);
+  }
+
+  return { startRound, hit };
 }
