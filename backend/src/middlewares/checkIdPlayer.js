26c042a99b583f7f4cf0535660d42a12f6717c4b
Shlomi <shlomikaufman91@gmail.com>
2026-08-20 08:14:01 +0300

    feat: add middleware for get player from DB

---
+++ backend/src/middlewares/checkIdPlayer.js
@@ -0,0 +1,18 @@
+export function middlewarePlayer(playerService) {
+  async function getPlayerId(req, res, next) {
+    try {
+      const playerId = req.headers["x-player-id"];
+
+      if (!playerId) {
+        return res.status(401).send({ error: `ID ${playerId} Not Exists` });
+      }
+
+      const player = await playerService.getPlayer({ id: playerId });
+      req.player = player;
+
+      next();
+    } catch (error) {
+      res.status(401).send(error.message);
+    }
+  }
+}
