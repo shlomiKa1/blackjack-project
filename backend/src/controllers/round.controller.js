910a6275f3d2c0e5263111a276a11bf7f9d8585c
Shlomi <shlomikaufman91@gmail.com>
2026-08-20 10:45:14 +0300

    fix: controller of round to fit middleware player

--- backend/src/controllers/round.controller.js
+++ backend/src/controllers/round.controller.js
@@ -1,28 +1,24 @@
 export default function createRoundController(roundService) {
   async function startRound(req, res) {
-    const playerId = req.headers["x-player-id"];
-    const round = await roundService.startRound(playerId, req.body);
+    const round = await roundService.startRound(req.player, req.body);
 
     res.status(201).send(round);
   }
 
   async function hit(req, res) {
-    const playerId = req.headers;
-    const hitGame = await roundService.hit(playerId);
+    const hitGame = await roundService.hit(req.player);
 
     res.send(hitGame);
   }
 
   async function stand(req, res) {
-    const playerId = req.headers;
-    const standGame = await roundService.stand(playerId);
+    const standGame = await roundService.stand(req.player);
 
     res.send(standGame);
   }
 
   async function myRound(req, res) {
-    const playerId = req.headers;
-    const myRoundGame = await roundService.myRound(playerId);
+    const myRoundGame = await roundService.myRound(req.player);
 
     res.send(myRoundGame);
   }
