9ab3e5ac3a01d2cf463b12ad3479aec6cc5503b7
Shlomi <shlomikaufman91@gmail.com>
2026-08-20 11:47:46 +0300

    Feat: add save players id at localstorage

--- frontend/app.js
+++ frontend/app.js
@@ -4,15 +4,13 @@ const HIT_API = `http://localhost:3000/hit`;
 const STAND_API = `http://localhost:3000/stand`;
 const GET_ROUND_API = `http://localhost:3000/my-round`;
 
-// const players = JSON.parse(localStorage.getItem("players")) ?? [];
+const players = JSON.parse(localStorage.getItem("players")) ?? [];
 
-// async function renderGame() {
-//   if (players.length === 0) {
-//     return await renderNewGame();
-//   } else {
-//     renderStartRound();
-//   }
-// }
+
+
+function savePlayers() {
+  localStorage.setItem("players", JSON.stringify(players));
+}
 
 // Read APIs
 async function startGamePost() {
