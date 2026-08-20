962189e8c1cd9bd899cc5dc8613d640812b3e331
Shlomi <shlomikaufman91@gmail.com>
2026-08-20 10:47:47 +0300

    feat: add the middleware of get player

--- backend/src/app.js
+++ backend/src/app.js
@@ -3,15 +3,22 @@ import createPlayerRoute from "./routes/player.route.js";
 import createRoundRoute from "./routes/round.route.js";
 import { errorHandler } from "./middlewares/errorhandler.js";
 import { logs } from "./middlewares/logs.js";
+import { middlewarePlayer } from "./middlewares/checkIdPlayer.js";
 
-export default function createApp({ playerController, roundController }) {
+export default function createApp({
+  playerController,
+  roundController,
+  playerService,
+}) {
   const app = express();
-  
+
   app.use(express.json());
   app.use(logs);
 
   app.use("/", createPlayerRoute(playerController));
-  app.use("/", createRoundRoute(roundController));
+
+  const authPlayer = middlewarePlayer(playerService);
+  app.use("/", authPlayer, createRoundRoute(roundController));
 
   app.use(errorHandler);
 
