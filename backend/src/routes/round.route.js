a420c26d552e22d56ec9608c3adba9f573f1d2e8
Shlomi <shlomikaufman91@gmail.com>
2026-08-20 10:42:31 +0300

    fix: express router at round

--- backend/src/routes/round.route.js
+++ backend/src/routes/round.route.js
@@ -1,8 +1,8 @@
 import express from "express";
 
 export default function createRoundRoute(roundController) {
-  const router = express();
-
+  const router = express.Router();
+  
   router.post("/start-round", roundController.startRound);
   router.post("/hit", roundController.hit);
   router.post("/stand", roundController.stand);
