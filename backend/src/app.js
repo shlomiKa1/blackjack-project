a041aa7f9b4ba8cf44065ae6b81b0c782fd61414
Shlomi <shlomikaufman91@gmail.com>
2026-08-21 17:00:47 +0300

    fix: route of get round player

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,4 +1,5 @@
 import express from "express";
+import cors from "cors";
 import createPlayerRoute from "./routes/player.route.js";
 import createRoundRoute from "./routes/round.route.js";
 import { errorHandler } from "./middlewares/errorhandler.js";
@@ -12,17 +13,18 @@ export default function createApp({
 }) {
   const app = express();
 
+  app.use(cors());
   app.use(express.json());
   app.use(logs);
-
+  
+  app.get("/", (req, res) => res.send("Welcome to BlackJuck game"));
+  
   app.use("/", createPlayerRoute(playerController));
-
+  
   const authPlayer = middlewarePlayer(playerService);
   app.use("/", authPlayer, createRoundRoute(roundController));
-
+  
   app.use(errorHandler);
-
-  app.get("/", (req, res) => res.send("Welcome to BlackJuck game"));
 
   return app;
 }
--- backend/src/routes/round.route.js
+++ backend/src/routes/round.route.js
@@ -6,7 +6,7 @@ export default function createRoundRoute(roundController) {
   router.post("/start-round", roundController.startRound);
   router.post("/hit", roundController.hit);
   router.post("/stand", roundController.stand);
-  router.post("/my-round", roundController.myRound);
+  router.get("/my-round", roundController.myRound);
 
   return router;
 }
